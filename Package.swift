5f9882c75c6313d24b08127e0c2cce686b1a4c75
aokimio <61457934+aokimio@users.noreply.github.com>
2022-09-27 13:15:39 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 #if os(Linux)
 let package = Package(
     name: "SwiftSocket",
+    version: "2.0.2",
     products: [
         .library(name: "SwiftSocket", targets: ["SwiftSocket"])
     ],
